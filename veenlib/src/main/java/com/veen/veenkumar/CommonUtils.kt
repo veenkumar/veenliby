21ad7d8a2ce72f603c26a2ef636f224dc84c16de
Veen Kumar <64082092+veenkumar@users.noreply.github.com>
2021-12-20 15:04:09 +0530

    Update CommonUtils.kt

--- veenlib/src/main/java/com/veen/veenkumar/CommonUtils.kt
+++ veenlib/src/main/java/com/veen/veenkumar/CommonUtils.kt
@@ -6,6 +6,20 @@ import com.google.android.material.snackbar.Snackbar
 
 class CommonUtils {
     companion object {
+        
+        fun Fragment.hideKeyboard() {
+            view?.let { activity?.hideKeyboard(it) }
+        }
+
+        fun Activity.hideKeyboard() {
+            hideKeyboard(currentFocus ?: View(this))
+        }
+
+        fun Context.hideKeyboard(view: View) {
+            val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
 
         fun longSnack(context: Context, msg: String) {
             val snackbar = Snackbar.make(
